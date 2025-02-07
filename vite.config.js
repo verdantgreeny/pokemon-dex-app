0a482ff73ce71c8cd5c157b73455f956d1cb10f6
verdantgreeny <verdantgreeny@gmail.com>
2025-02-07 16:25:12 +0900

    script: vercel.json 생성

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-		alias: [
-			{ find: "@", replacement: resolve(__dirname, "src") },
-			{ find: "@assets", replacement: resolve(__dirname, "src/assets") },
-			{
-				find: "@components",
-				replacement: resolve(__dirname, "src/components"),
-			},
-			{
-				find: "@pages",
-				replacement: resolve(__dirname, "src/pages"),
-			},
-		],
-	},
-})
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
