1152917efdf8fdcd5eec0fce0d04620e022f7e53
verdantgreeny <verdantgreeny@gmail.com>
2025-02-07 15:55:41 +0900

    fix: vite.config.js에 절대경로 수정

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+		alias: [
+			{ find: "@", replacement: resolve(__dirname, "src") },
+			{ find: "@assets", replacement: resolve(__dirname, "src/assets") },
+			{
+				find: "@components",
+				replacement: resolve(__dirname, "src/components"),
+			},
+			{
+				find: "@pages",
+				replacement: resolve(__dirname, "src/pages"),
+			},
+		],
+	},
 })
