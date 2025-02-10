f83024d0491356364b582f19d56df7cfcd29b691
verdantgreeny <verdantgreeny@gmail.com>
2025-02-10 17:18:24 +0900

    update: 이미지 클릭이 아닌 카드 클릭시 상세페이지 이동으로 변경

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,21 +1,27 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { StPokemonCard } from "../styles/styledComponents";
 import Button from "./Button";
 import { PokemonContext } from "../contexts/PokemonContext";
 
 const PokemonCard = ({ pokemon, isDashboard }) => {
   const { onAddHandler, onDeleteHandler } = useContext(PokemonContext);
+  const navigate = useNavigate();
   return (
-    <StPokemonCard>
+    <StPokemonCard
+      onClick={(e) => {
+        if (e.target.tagName === "BUTTON") {
+          return;
+        }
+        navigate(`/detail?id=${pokemon.id}`);
+      }}
+    >
       <div className="pokemon-name">
         {" "}
         <span> {String(pokemon.id).padStart(3, "0")} </span>
         {pokemon.korean_name}
       </div>
-      <Link to={`/detail?id=${pokemon.id}`}>
         <img src={pokemon.img_url} alt={pokemon.korean_name} />
-      </Link>
       {!isDashboard ? (
         <Button
           color={pokemon.isSelected && "yellow"}
